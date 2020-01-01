b3a5b7d9934fb39a43680341e20d3cb0519a84bd
João Gardenberg <joaogardenberg@gmail.com>
2020-01-01 06:46:13 -0300

    Finished facebook strategy

--- backend/src/middlewares/facebookStrategy.js
+++ backend/src/middlewares/facebookStrategy.js
@@ -5,17 +5,96 @@ const {
   FACEBOOK_CALLBACK_URL
 } = require('../config')
 
+FacebookStrategy.prototype.authorizationParams = function(options) {
+  var params = {}
+
+  if (options.display) {
+    params.display = options.display
+  }
+
+  if (options.authType) {
+    params.auth_type = options.authType
+  }
+
+  if (options.authNonce) {
+    params.auth_nonce = options.authNonce
+  }
+
+  console.log(params)
+  return params
+}
+
 module.exports = (passport, User) => {
   passport.use(
     new FacebookStrategy(
       {
         clientID: FACEBOOK_CLIENT_ID,
         clientSecret: FACEBOOK_CLIENT_SECRET,
-        callbackURL: FACEBOOK_CALLBACK_URL
+        callbackURL: FACEBOOK_CALLBACK_URL,
+        profileFields: ['id', 'displayName', 'email', 'photos']
       },
-      (accessToken, refreshToken, profile, done) => {
-        console.log(request, accessToken, refreshToken, profile, done)
-        done(null, false)
+      (_, __, { id: facebookId, displayName: name, emails, photos }, done) => {
+        const { value: picture } =
+          photos && photos.length
+            ? photos.find(({ value }) => value)
+            : { value: undefined }
+
+        const { value: oauthEmail } =
+          emails && emails.length
+            ? emails.find(({ value }) => value)
+            : { value: undefined }
+
+        User.findOne({ facebookId, loginProvider: 'facebook' }, (err, user) => {
+          if (err) {
+            return done(err, user, { message: 'Something went wrong' })
+          }
+
+          if (user) {
+            if (
+              (name && user.name !== name) ||
+              (picture && (user.picture || {}).url !== picture) ||
+              (oauthEmail && user.oauthEmail !== oauthEmail)
+            ) {
+              const newName = name || user.name
+              const newPicture =
+                picture && (user.picture || {}).url !== picture
+                  ? { url: picture, alt: `${name || 'User'}'s picture` }
+                  : user.picture
+              const newOauthEmail = oauthEmail || user.oauthEmail
+
+              user
+                .set({
+                  name: newName,
+                  picture: newPicture,
+                  oauthEmail: newOauthEmail
+                })
+                .save((_err, _user) => {
+                  _err
+                    ? done(_err, _user, { message: 'Something went wrong' })
+                    : done(null, _user, { message: 'Signed in successfully' })
+                })
+            } else {
+              done(null, user, { message: 'Signed in successfully' })
+            }
+          } else {
+            User.create(
+              {
+                name,
+                picture: picture
+                  ? { url: picture, alt: `${name || 'User'}'s picture` }
+                  : undefined,
+                facebookId,
+                oauthEmail,
+                loginProvider: 'facebook'
+              },
+              (_err, _user) => {
+                _err
+                  ? done(_err, _user, { message: 'Something went wrong' })
+                  : done(null, _user, { message: 'Signed in successfully' })
+              }
+            )
+          }
+        })
       }
     )
   )
