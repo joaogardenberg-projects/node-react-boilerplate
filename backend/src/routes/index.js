3adb099ac772dd38f4131295771d8410068247b9
João Gardenberg <joaogardenberg@gmail.com>
2020-01-01 01:33:04 -0300

    Removed not found route redirection

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,7 @@
 const authRoutes = require('./auth')
 const userRoutes = require('./users')
-const notFoundRoute = require('./notFound')
 
 module.exports = (app) => {
   authRoutes(app)
   userRoutes(app)
-
-  notFoundRoute(app) // Has to be last
 }
