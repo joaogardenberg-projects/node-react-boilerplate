ef0d3381ae79afab18f47026a5ed5ea515ea83c4
João Gardenberg <joaogardenberg@gmail.com>
2020-01-02 12:13:34 -0300

    Renamed to BackendTests

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
-import BackendTest from './BackendTest'
+import BackendTests from './BackendTests'
 import OAuthCallback from './auth/OAuthCallback'
 
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={BackendTest} />
+      <Route exact path="/" component={BackendTests} />
       <Route exact path="/auth/callback" component={OAuthCallback} />
     </BrowserRouter>
   )
