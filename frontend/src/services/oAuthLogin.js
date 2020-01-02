498be254cee611270aad1abdda4a14e1cebd9b4e
João Gardenberg <joaogardenberg@gmail.com>
2020-01-02 11:24:55 -0300

    Added OAuth login popup

---
+++ frontend/src/services/oAuthLogin.js
@@ -0,0 +1,23 @@
+import { BASE_URL } from './api'
+
+export default (type) =>
+  new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      reject(null)
+    }, 30000)
+
+    const _resolve = (event) => {
+      clearTimeout(timeout)
+      resolve(event)
+    }
+
+    window.removeEventListener('message', _resolve)
+
+    window.open(
+      `${BASE_URL}/auth/${type}`,
+      `${type} login`,
+      'toolbar=no, menubar=no, width=600, height=700, top=100, left=100'
+    )
+
+    window.addEventListener('message', (event) => _resolve(event), false)
+  })
