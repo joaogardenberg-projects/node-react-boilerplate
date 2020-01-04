e11e941ae8abfe956cef7db720a547145a5886d2
João Gardenberg <joaogardenberg@gmail.com>
2020-01-04 12:28:00 -0300

    Error handling improvements

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -1,6 +1,10 @@
 import API from '../services/api'
 import oAuthSignIn from '../services/oAuthSignIn'
-import { setAuthToken, removeAuthToken } from '../services/authToken'
+import {
+  getAuthToken,
+  setAuthToken,
+  removeAuthToken
+} from '../services/authToken'
 import {
   GET_CURRENT_USER_SENT,
   GET_CURRENT_USER_SUCCEEDED,
@@ -22,16 +26,17 @@ import {
 export const getCurrentUser = () => async (dispatch) => {
   try {
     dispatch({ type: GET_CURRENT_USER_SENT })
-    const { status, data: user } = await API.get('/auth')
 
-    if (!user || status !== 200) {
-      throw new Error(user)
+    if (!getAuthToken()) {
+      throw new Error('Not logged in')
     }
 
+    const { data: user } = await API.get('/auth')
+
     dispatch({ type: GET_CURRENT_USER_SUCCEEDED, payload: user })
-  } catch (e) {
+  } catch (error) {
     removeAuthToken()
-    dispatch({ type: GET_CURRENT_USER_FAILED })
+    dispatch({ type: GET_CURRENT_USER_FAILED, payload: { error } })
   }
 }
 
@@ -40,19 +45,14 @@ export const signInLocal = ({ email, password }) => async (dispatch) => {
     dispatch({ type: SIGN_IN_LOCAL_SENT })
 
     const {
-      status,
       data: { user, token }
     } = await API.post('/auth', { email, password })
 
-    if (!user || !token || status !== 200) {
-      throw new Error({ user, token })
-    }
-
     setAuthToken(token)
     dispatch({ type: SIGN_IN_LOCAL_SUCCEEDED, payload: user })
-  } catch (e) {
+  } catch (error) {
     removeAuthToken()
-    dispatch({ type: SIGN_IN_LOCAL_FAILED })
+    dispatch({ type: SIGN_IN_LOCAL_FAILED, payload: { error } })
   }
 }
 
@@ -60,28 +60,21 @@ export const signInGoogle = () => async (dispatch) => {
   try {
     dispatch({ type: SIGN_IN_GOOGLE_SENT })
 
-    const { status: statusOAuth, data: queryString } = await oAuthSignIn(
-      'google'
-    )
+    const queryString = await oAuthSignIn('google')
 
-    if (!queryString || statusOAuth !== 200) {
-      throw new Error(queryString)
+    if (!queryString) {
+      throw new Error('Something went wrong')
     }
 
     const {
-      status,
       data: { user, token }
     } = await API.get(`/auth/google/callback${queryString}`)
 
-    if (!user || !token || status !== 200) {
-      throw new Error({ user, token })
-    }
-
     setAuthToken(token)
     dispatch({ type: SIGN_IN_GOOGLE_SUCCEEDED, payload: user })
-  } catch (e) {
+  } catch (error) {
     removeAuthToken()
-    dispatch({ type: SIGN_IN_GOOGLE_FAILED })
+    dispatch({ type: SIGN_IN_GOOGLE_FAILED, payload: { error } })
   }
 }
 
@@ -89,28 +82,21 @@ export const signInFacebook = () => async (dispatch) => {
   try {
     dispatch({ type: SIGN_IN_FACEBOOK_SENT })
 
-    const { status: statusOAuth, data: queryString } = await oAuthSignIn(
-      'facebook'
-    )
+    const queryString = await oAuthSignIn('facebook')
 
-    if (!queryString || statusOAuth !== 200) {
-      throw new Error(queryString)
+    if (!queryString) {
+      throw new Error('Something went wrong')
     }
 
     const {
-      status,
       data: { user, token }
     } = await API.get(`/auth/facebook/callback${queryString}`)
 
-    if (!user || !token || status !== 200) {
-      throw new Error({ user, token })
-    }
-
     setAuthToken(token)
     dispatch({ type: SIGN_IN_FACEBOOK_SUCCEEDED, payload: user })
-  } catch (e) {
+  } catch (error) {
     removeAuthToken()
-    dispatch({ type: SIGN_IN_FACEBOOK_FAILED })
+    dispatch({ type: SIGN_IN_FACEBOOK_FAILED, payload: { error } })
   }
 }
 
@@ -119,17 +105,16 @@ export const signOut = () => async (dispatch) => {
     dispatch({ type: SIGN_OUT_SENT })
 
     const {
-      status,
       data: { success }
     } = await API.delete('/auth')
 
-    if (!success || status !== 200) {
-      throw new Error(success)
+    if (!success) {
+      throw new Error('Something went wrong')
     }
 
     removeAuthToken()
     dispatch({ type: SIGN_OUT_SUCCEEDED })
-  } catch (e) {
-    dispatch({ type: SIGN_OUT_FAILED })
+  } catch (error) {
+    dispatch({ type: SIGN_OUT_FAILED, payload: { error } })
   }
 }
