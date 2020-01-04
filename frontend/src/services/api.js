7599d73c8ec0f2b15a99edafea16be1da956392b
João Gardenberg <joaogardenberg@gmail.com>
2020-01-04 12:30:48 -0300

    Added query string option to api request maker

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import omit from 'lodash/omit'
 import { getAuthToken } from './authToken'
 import config from '../config'
 
@@ -12,12 +13,35 @@ export const patch = (path, options) => request('patch', path, options)
 
 export const _delete = (path, options) => request('delete', path, options)
 
-const request = (method, path, options) =>
-  axios({
+const addQueryString = (path, queryObject = {}) => {
+  if (!queryObject || !Object.keys(queryObject).length) {
+    return path
+  }
+
+  const queryString = Object.keys(queryObject)
+    .filter((key) => queryObject[key])
+    .map((key) => `${key}=${queryObject[key]}`)
+    .join('&')
+
+  if (!queryString) {
+    return path
+  }
+
+  return `${path}?${queryString}`
+}
+
+const request = (method, path, options = {}) => {
+  const data = omit(options, 'queryString')
+
+  return axios({
     method,
-    url: `${config.BASE_BACKEND_URL}${path}`,
-    data: options,
+    data,
+    url: `${config.BASE_BACKEND_URL}${addQueryString(
+      path,
+      options.queryString
+    )}`,
     headers: { Authorization: `Bearer ${getAuthToken()}` }
   })
+}
 
 export default { get, post, put, patch, delete: _delete }
