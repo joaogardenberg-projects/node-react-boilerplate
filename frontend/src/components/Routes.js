6cd96dfb8afb6f6b20fedfd3b9e18c3a431b3912
João Gardenberg <joaogardenberg@gmail.com>
2020-01-04 12:33:54 -0300

    Getting auth validation in the routes file

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
-import BackendTests from './BackendTests'
+import { getCurrentUser } from '../actions'
+import BackendTests from './backendTests/BackendTests'
 import OAuthCallback from './auth/OAuthCallback'
 
-const Routes = () => {
-  return (
-    <BrowserRouter>
-      <Route exact path="/" component={BackendTests} />
-      <Route exact path="/auth/callback" component={OAuthCallback} />
-    </BrowserRouter>
-  )
+class Routes extends Component {
+  componentDidMount() {
+    const {
+      auth: { isFetching, isPresent }
+    } = this.props
+
+    !isFetching && !isPresent && this.props.getCurrentUser()
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={BackendTests} />
+        <Route exact path="/auth/callback" component={OAuthCallback} />
+      </BrowserRouter>
+    )
+  }
 }
 
-export default Routes
+Routes.propTypes = {
+  auth: PropTypes.object.isRequired,
+  getCurrentUser: PropTypes.func.isRequired
+}
+
+const mapStateToProps = ({ auth }) => ({ auth })
+
+export default connect(mapStateToProps, { getCurrentUser })(Routes)
