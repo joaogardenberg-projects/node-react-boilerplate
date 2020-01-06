711cf1474b147b7ebcff9442db3e0329f72e0959
João Gardenberg <joaogardenberg@gmail.com>
2020-01-06 17:48:13 -0300

    Removed redux devtools

--- frontend/src/providers/Redux.js
+++ frontend/src/providers/Redux.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reduxThunk from 'redux-thunk'
 import reducers from '../reducers'
 
 const Redux = ({ children, initialState = {} }) => {
-  const store = createStore(
-    reducers,
-    initialState,
-    compose(
-      applyMiddleware(reduxThunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__
-        ? window.__REDUX_DEVTOOLS_EXTENSION__()
-        : (f) => f
-    )
-  )
+  const store = createStore(reducers, initialState, applyMiddleware(reduxThunk))
   return <Provider store={store}>{children}</Provider>
 }
 
