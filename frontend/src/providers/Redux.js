1e50c636965ef3456f19691d676688d6ba10f9e1
João Gardenberg <joaogardenberg@gmail.com>
2020-01-07 05:12:19 -0300

    Added redux devtools support

--- frontend/src/providers/Redux.js
+++ frontend/src/providers/Redux.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import reduxThunk from 'redux-thunk'
 import reducers from '../reducers'
 
 const Redux = ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(reduxThunk))
+  const store = createStore(
+    reducers,
+    initialState,
+    compose(
+      applyMiddleware(reduxThunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : (f) => f
+    )
+  )
   return <Provider store={store}>{children}</Provider>
 }
 
