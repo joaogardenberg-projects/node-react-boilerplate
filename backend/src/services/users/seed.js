c7e2065df0bef434fdbe0aa17d1d1d2ec1b8833d
João Gardenberg <joaogardenberg@gmail.com>
2020-08-30 09:18:08 -0300

    Initial user creation warning

--- backend/src/services/users/seed.js
+++ backend/src/services/users/seed.js
@@ -2,6 +2,8 @@ const User = require('models/mongo/user')
 
 module.exports = async () => {
   if (!(await User.countDocuments({}))) {
+    console.log('No users found in the db. Creating initial user...')
+
     await new User({
       name: 'Initial User',
       email: 'initial@user.com',
