09358a7b486cb7fa21c9b49f2def72d073e68f3b
João Gardenberg <joaogardenberg@gmail.com>
2020-08-30 07:14:26 -0300

    Also moved envs to .env files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const { PORT } = require('config')
 const addMiddlewares = require('middlewares')
